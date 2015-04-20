40d76117e60004293d828e350a35c3db53da1eff
Sander Smits <sander.smits@nelen-schuurmans.nl>
2015-04-20 20:34:27 +0200

    Renamed files.

---
+++ wmsrp.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "wmsrp"
+	app.Usage = "wms reverse proxy for the 3di scalability architecture"
+	app.Version = "0.0.1"
+	app.Authors = []cli.Author{
+		cli.Author{Name: "Sander Smits", Email: ""},
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "port, p",
+			Value: "5050",
+			Usage: "port this reverse proxy serves on",
+		},
+		cli.StringFlag{
+			Name:  "redis-host",
+			Value: "127.0.0.1",
+			Usage: "redis server host",
+		},
+		cli.StringFlag{
+			Name:  "redis-port",
+			Value: "6379",
+			Usage: "redis server port",
+		},
+		cli.StringFlag{
+			Name:  "wms-port",
+			Value: "5000",
+			Usage: "wms server port",
+		},
+		cli.BoolFlag{
+			Name:  "use-cache",
+			Usage: "cache session keys (use for development environments)",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		port := c.String("port")
+		redisHost := c.String("redis-host")
+		redisPort := c.String("redis-port")
+		wmsPort := c.String("wms-port")
+		useCache := c.Bool("use-cache")
+
+		wmsRevProxy := wmsReverseProxy(redisHost, redisPort, wmsPort, useCache)
+
+		log.Println("starting wms reverse proxy on port", port)
+		log.Println("using redirect info from redis server on", strings.Join([]string{redisHost, redisPort}, ":"))
+		log.Println("redirecting to wms servers on port", wmsPort)
+
+		err := http.ListenAndServe(":"+port, wmsRevProxy)
+		if err != nil {
+			log.Fatal("http.ListenAndServe:", err)
+		}
+	}
+	app.Run(os.Args)
+}
