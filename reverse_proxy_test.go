f9dfe3304e8cc754c87b09fb75ee8e75c5d7c134
Sander Smits <sander.smits@nelen-schuurmans.nl>
2015-04-23 16:30:30 +0200

    Workaround testify.assert issue.

--- reverse_proxy_test.go
+++ reverse_proxy_test.go
@@ -9,5 +9,7 @@ import (
 func TestRedisPool(t *testing.T) {
 	pool := newRedisPool("localhost:6379", "")
 	assert.Equal(t, 3, pool.MaxIdle)
-	assert.Equal(t, 240000000000, pool.IdleTimeout) // 240 seconds
+	// need to cast to int, because the values are different types (int32 and int)
+	// see issue https://github.com/stretchr/testify/issues/155
+	assert.Equal(t, int(240000000000), int(pool.IdleTimeout)) // 240 seconds
 }
