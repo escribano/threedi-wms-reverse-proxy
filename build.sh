a9e4f1d6cbd1acf12ed4424310f21276d2768684
Sander Smits <sander.smits@nelen-schuurmans.nl>
2015-11-07 20:03:46 +0100

    Add comments to build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# build script that injects version from ``version`` file into the ``main.Version`` variable, so that the version gets compiled into the binary
+# also see: http://stackoverflow.com/questions/28459102/golang-compile-environment-variable-into-binary
 export VERSION=`cat version`
 go build -ldflags "-X main.Version=$VERSION" -o wmsrp .
 
